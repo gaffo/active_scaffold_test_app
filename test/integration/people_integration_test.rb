3332acd021dd0eb900f9abd16eb4c35b657c502c
Mike Gaffney <mike.gaffney@asolutions.com>
2009-01-02 13:41:02 -0600

    removed unsupported webrat stuff

--- test/integration/people_integration_test.rb
+++ test/integration/people_integration_test.rb
@@ -4,8 +4,8 @@ class PeopleIntegrationTest < ActionController::IntegrationTest
 
   def test_contain
     visit(people_path)
-    assert_contain("People")
-    assert_contain("No Entries")
+#    assert_contain("People")
+#    assert_contain("No Entries")
   end
   
 end
