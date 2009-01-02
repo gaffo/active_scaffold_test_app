133252e8dacc0f6f5dda74f9813787fb74635638
Mike Gaffney <mike.gaffney@asolutions.com>
2009-01-02 13:54:11 -0600

    working on first test

--- test/integration/people_integration_test.rb
+++ test/integration/people_integration_test.rb
@@ -1,11 +1,15 @@
 require 'test_helper'
 
 class PeopleIntegrationTest < ActionController::IntegrationTest
-
-  def test_contain
-    visit(people_path)
-#    assert_contain("People")
-#    assert_contain("No Entries")
+  
+  context "context no records" do
+    setup do 
+      visit(people_path)
+      click_link("Create New")
+      fill_in('Name', :with => "Amos King")
+      click_button('Create')
+    end
+    should_change "Person.count", :by => 1
   end
   
 end
